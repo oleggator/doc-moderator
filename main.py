7fb44b5ca55a33031b4365973ec0a816a3b64f8a
oleggator <oleg@utkin.xyz>
2018-12-13 14:02:39 +0300

    fix codestyle

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from docx import Document
 from os import path
 import argparse
 
+
 def find_words(target_words_file_path, article_path, output_path, ld):
     extension = path.splitext(article_path)[1]
     if extension == '.docx':
         document = Document(article_path)
         article_words = set({})
         for paragraph in document.paragraphs:
-            for word in fokin_split(paragraph.text):
+            for word in split(paragraph.text):
                 article_words.add(word)
     else:
         with open(article_path, 'r') as file:
-            article_words = set(fokin_split(file.read()))
+            article_words = set(split(file.read()))
 
     with open(target_words_file_path, 'r') as file:
-        target_words = set(fokin_split(file.read()))
-
+        target_words = set(split(file.read()))
 
     log = open(output_path, 'w')
 
@@ -32,20 +32,31 @@ def find_words(target_words_file_path, article_path, output_path, ld):
 
     log.write('\n'.join(found_words) + '\n')
 
-def fokin_split(test):
+
+def split(test):
     pun = '!"#$%&\'()*+,–./:;<=>?@[\\]^_`{|}~”'
-    translator = str.maketrans(pun, ' ' * len(pun)) #map punctuation to space
+    translator = str.maketrans(pun, ' ' * len(pun))
     return test.lower().translate(translator).split()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='find words in document')
-    parser.add_argument('-d', required=True, action='store', dest='target_words',
+    parser.add_argument('-d', required=True,
+                        action='store',
+                        dest='target_words',
                         help='dictionary (txt)')
-    parser.add_argument('-a', required=True, action='store', dest='article',
+    parser.add_argument('-a', required=True,
+                        action='store',
+                        dest='article',
                         help='article (txt or docx)')
-    parser.add_argument('-o', required=True, action='store', dest='output',
+    parser.add_argument('-o', required=True,
+                        action='store',
+                        dest='output',
                         help='found words output file')
-    parser.add_argument('-l', required=True, action='store', dest='distance', type=int,
+    parser.add_argument('-l', required=True,
+                        action='store',
+                        dest='distance',
+                        type=int,
                         help='editor distance')
     args = parser.parse_args()
 
